450baa113da46f35e97ec44d26053b2cb02be7bc
unknown <joannasmerea@gmail.com>
2021-02-08 19:22:37 +0100

    adding storybook

--- src/components/InvestmentPredicted.stories.js
+++ src/components/InvestmentPredicted.stories.js
@@ -1,8 +1,40 @@
 import InvestmentPredicted from './InvestmentPredicted.vue'
 import { storiesOf } from '@storybook/vue';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, array } from '@storybook/addon-knobs';
 
-const stories = storiesOf('InvestmentPredicted', module)
+const stories = storiesOf('InvestmentPredicted', module);
+
+const label = 'Dataset';
+const defaultValue = [{
+  month: 'Jan',
+  high: 75,
+  low: 35
+},
+{
+  month: 'Feb',
+  high: 47,
+  low: 22
+},
+{
+  month: 'Mar',
+  high: 80,
+  low: 53
+},
+{
+  month: 'Apr',
+  high: 50,
+  low: 30
+},
+{
+  month: 'May',
+  high: 70,
+  low: 50
+},
+{
+  month: 'Jun',
+  high: 80,
+  low: 10
+}]
 
 // Add the `withKnobs` decorator to add knobs support to your stories.
 stories.addDecorator(withKnobs);
@@ -10,13 +42,14 @@ stories.addDecorator(withKnobs);
 stories.add('Investment Predicted component', () => ({
  components: { InvestmentPredicted },
  props: { 
-  msg: {
+  
+  dataset: {
    /* 
    * The text(label, defaultValue) method from Knobs allows you to receive 
    * value dynamically in the Storybook Ui.
    */
-  default: text('Text', 'Hello Storybook')
+  default: array(label, defaultValue)
 }
  },
- template: '<InvestmentPredicted :msg="msg"/>'
+ template: '<InvestmentPredicted :dataset="dataset"/>'
 }));
